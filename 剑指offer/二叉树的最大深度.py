c924e76cdafd119c0fc112ff6f20e32cab880435
xujunwei <ryanxjw@163.com>
2021-06-24 14:43:55 +0800

    6.23 网易面试整合

---
+++ 剑指offer/二叉树的最大深度.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+# _*_coding=utf-8 _*_
+# @author junwei
+# @date 2021/6/24 14:42
+# description
+# 给定一个二叉树，找出其最大深度。
+#
+# 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+#
+# 说明: 叶子节点是指没有子节点的节点。
+#
+# 示例：
+# 给定二叉树 [3,9,20,null,null,15,7]，
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if root is None:
+            return 0
+        else:
+            left_height = self.maxDepth(root.left)
+            right_height = self.maxDepth(root.right)
+            return max(left_height, right_height) + 1

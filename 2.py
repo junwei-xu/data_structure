7ba0bac7ee065ad1887b6ce12e962969eccffbba
xujunwei <ryanxjw@163.com>
2021-12-04 20:11:55 +0800

    del

--- 2.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-# _*_coding=utf-8 _*_
-# @author junwei
-# @date 2021/8/21 15:48
-# description
--- hw1.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-# _*_coding=utf-8 _*_
-# @author junwei
-# @date 2021/9/10 17:26
-# description
